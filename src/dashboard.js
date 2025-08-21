bd6ab7f24c417c8ec0ccf68a7020c0e54d2f2706
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-08-22 00:56:01 +0530

    Dollar removed

--- src/dashboard.js
+++ src/dashboard.js
@@ -248,9 +248,9 @@ class DashboardManager {
     }
 
     formatCurrency(amount) {
-        return new Intl.NumberFormat('en-US', {
+        return new Intl.NumberFormat('en-IN', {
             style: 'currency',
-            currency: 'USD',
+            currency: 'INR',
             minimumFractionDigits: 0,
             maximumFractionDigits: 2
         }).format(amount);
